ab03d2e8b8d4a8b501508ee8939db9a06e7ba9a8
Yanis <yanis.fourel@gmail.com>
2019-05-11 13:51:47 +0200

    meta file cleaning

---
+++ Assets/Scripts/Parralax.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Parralax : MonoBehaviour
+{
+    /// <summary>
+    /// Unit by seconds
+    /// </summary>
+    public float Speed = 1f;
+
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector2(-Speed * Time.deltaTime, 0));
+    }
+}
